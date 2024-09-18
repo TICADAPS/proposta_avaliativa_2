b66113d044f02f05ac0adfa5817d5705e72fbd11
unknown <marcelo.farias@agenciasus.org.br>
2024-09-18 16:24:35 -0300

    segundo commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'admin/sala-reuniao',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e518751ea56cd09f7a13e0d7f3bc37205a9640dd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'admin/sala-reuniao' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e518751ea56cd09f7a13e0d7f3bc37205a9640dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
